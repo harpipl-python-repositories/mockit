1ef4c16e153fedc3fb1183315b05bc7925a08db2
harpipl <arkadiusz.adamczak@gmail.com>
2025-03-31 21:18:01 +0200

    Added serializers & urls

--- repository/models.py
+++ repository/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
+class ProjectType(models.TextChoices):
+    REST = 'REST', 'REST'
+    SOAP = 'SOAP', 'SOAP'
+
+
+class Project(models.Model):
+    logical_id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=20, choices=ProjectType.choices, default=ProjectType.REST)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'type'], name='unique_name_type')
+        ]
 # Create your models here.
---
+++ repository/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import ProjectViewSet
+
+router = routers.DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
