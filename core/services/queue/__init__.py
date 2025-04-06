1b53989aa2db07347f7e1e49c95542f50a56c86c
harpipl <arkadiusz.adamczak@gmail.com>
2025-04-06 10:51:29 +0200

    Repaired __init__.py

--- core/services/queue/__init__.py
+++ core/services/queue/__init__.py
@@ -1,7 +1,8 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from azure.servicebus import ServiceBusReceivedMessage
 
 
 class AbstractQueueHandler(ABC):
+    @abstractmethod
     async def handle_message(self, message: ServiceBusReceivedMessage):
         pass
